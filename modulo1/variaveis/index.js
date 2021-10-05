c9e9752330dae64980e40809ef945cd0d324e7b0
JoaoGMMaretti <leffymaretti@gmail.com>
2021-10-05 11:41:21 -0300

    Atividade-Variaveis

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,63 @@
+
+//Respostas Dos Exercicios de interpretação
+
+//Exercicio 1: 1º:10 2º:10,5
+
+//Exercicio 2: 10 10 10
+
+//Exercicio 3: horasTrabalhadasPorDia = P salarioDiario = T
+
+
+
+
+//Respostas dos exercicios de Codigo
+
+//Exercicico 1-D foi escrito string devido ao uso do prompt
+//Exercicio  1-F Apos atribuir os valores a idade a cor no console foi alterada
+
+//Exercicio 2 
+//Estou usando preto hoje ? true
+//Assisti a aula hoje de manha ? false
+//Estou Gostando do Curso ? true
+
+
+//Codigo 1
+/*const nome = prompt("Qual é o seu nome ?")
+let idadetexto = prompt("Qual a sua idade ?") 
+let idade = Number(idadetexto)
+
+console.log(nome)
+console.log(idadetexto)
+console.log(typeof nome) 
+console.log(typeof idade)
+console.log("Ola", nome, "você tem", idade, "anos")
+
+
+
+
+//Codigo 2
+/*const cor = true
+const aula = false
+const curso = true
+
+console.log("Estou usando preto hoje ?", cor )
+console.log("Assisti a aula hoje de manha ?", aula )
+console.log("Estou Gostando do Curso ?", curso )
+*/
+
+
+
+
+//Codigo 3
+/* 
+let a = 10
+let b = 25
+let c = a
+
+
+// Depois de trocados, teremos o seguinte resultado:
+console.log("O novo valor de a é", b) // O novo valor de a é 25
+console.log("O novo valor de b é", c) // O novo valor de b é 10
+
+
+*/
